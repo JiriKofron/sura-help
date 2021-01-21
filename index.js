fd4d37813e5bcc13fb84c18840c523b69b666163
JiriKofron <51482900+JiriKofron@users.noreply.github.com>
2021-01-21 21:42:15 +0100

    Description of the process

--- index.js
+++ index.js
@@ -27,15 +27,14 @@ app.get('/', (req, res) => {
 app.get('/create', (req, res) => {
   res.send('cesta je');
 });
-
+// create basic sceleton of new project
 app.post('/create', (req, res) => {
-  console.log(req.body);
-  console.log(Project);
   const project = new Project({
     _id: new mongoose.Types.ObjectId(),
     title: req.body.title,
     comment: req.body.comment,
   });
+  // save the project and during the save, create new Title binded to project by "project._id", thanks to binding it is possible to delete Title objects which belong to the delete Project
   project.save(() => {
     const title = new Title({
       _id: new mongoose.Types.ObjectId(),
@@ -44,23 +43,14 @@ app.post('/create', (req, res) => {
     });
     title.save();
 
+    // Finding the created project, so the Title can be added to titleList array
     Project.findOne({ title: req.body.title }, (err, project) => {
       project.titlesList = title;
       project.save();
     });
   });
 
-  res.send('databaze zapsana');
-});
-
-app.post('/title', (req, res) => {
-  Title.create({ newTitle: req.body.title });
-  res.send('title vytvořen');
-});
-
-app.get('/populate', (req, res) => {
-  Project.findOne({ title: 'johoho5' }).populate('titlesList');
-  res.send('populate');
+  res.send('Object created in database');
 });
 
 //Here I want to delete the project with its refernced (delete = project + its titles list)
@@ -71,14 +61,18 @@ app.delete('/delete/:id/', (req, res) => {
     if (!err) {
       console.log(doc);
       console.log(id);
-      res.send('smazano');
+      res.send('deleted');
     } else {
       console.log('Error in project delete :' + err);
     }
   });
+
+  // Looking for Project id in the Title object in project value, if more Titles apply, the deleteMany method allows to delete them all
   Title.deleteMany({ project: id }, (err, doc1) => {
     if (!err) {
       console.log(doc1);
+    } else {
+      console.log('Error in title delete: ' + err);
     }
   });
 });
